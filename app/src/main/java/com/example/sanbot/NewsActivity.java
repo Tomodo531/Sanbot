072abbcc90a73e368a09bc0c16f3f2f5ce9d8042
sami0596 <gamesteamdk@hotmail.com>
2019-06-24 13:06:37 +0200

    Update NewsActivity.java
    Added comments

--- app/src/main/java/com/example/sanbot/NewsActivity.java
+++ app/src/main/java/com/example/sanbot/NewsActivity.java
@@ -60,11 +60,12 @@ public class NewsActivity extends AppCompatActivity {
         new ProcessInBackground().execute();
     }
 
+
     public InputStream getInputStream(URL url)
     {
         try
         {
-
+            //Trying to open a connection to the url (the rss url)
             return url.openConnection().getInputStream();
         }
         catch (IOException e)
@@ -82,7 +83,7 @@ public class NewsActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-
+            //The loading message, before the news shows up
             progressDialog.setMessage("Loader Nyheder...Vent Venligst...");
             progressDialog.show();
         }
@@ -92,34 +93,39 @@ public class NewsActivity extends AppCompatActivity {
 
             try
             {
-                //Getting news data from the website via rss
+                //Getting news data from the news' rss website
                 URL url = new URL("http://feeds.tv2.dk/nyhederne_seneste/rss");
 
-
+                //create a new pool parser factory object.
+                //Basically help us to retrieve the data from the xml document
                 XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
 
-
+                //Specifies whether the parser produced by the factory
+                //will provide support for XML namespaces or not
                 factory.setNamespaceAware(false);
 
-
+                //Creating a new pool parser
                 XmlPullParser sa = factory.newPullParser();
 
-
+                //new input stream, which makes a new connection
+                //and we can start reading using the XML pool parser object
+                //start reading from the URL XML document that is utf-8 encoded
                 sa.setInput(getInputStream(url), "UTF_8");
 
-
+                //Boolean to tell when inside the item tag in the rss url
                 boolean insideItem = false;
 
-
+                //When start reading from it, when call getEventType, it returns the type of the current event
                 int eventType = sa.getEventType();
 
-                //Keep showing the newsfeed, until reaching end
+                //Keep showing the newsfeed, until reaching the end of the document
                 while (eventType != XmlPullParser.END_DOCUMENT)
                 {
-
+                    //if it's a start of specific tags
                     if (eventType == XmlPullParser.START_TAG)
                     {
-
+                        //if item tag is "item",
+                        //We will be inside the item tag
                         if (sa.getName().equalsIgnoreCase("item"))
                         {
                             insideItem = true;
@@ -137,6 +143,8 @@ public class NewsActivity extends AppCompatActivity {
 
                         else if (sa.getName().equalsIgnoreCase("link"))
                         {
+                            //Add new item tag next to the other, if already inside another item tag
+
                             if (insideItem)
                             {
 
@@ -144,25 +152,28 @@ public class NewsActivity extends AppCompatActivity {
                             }
                         }
                     }
-
+                    //If reaching end and still inside item tag, we will stop reading inside the item tag
                     else if (eventType == XmlPullParser.END_TAG && sa.getName().equalsIgnoreCase("item"))
                     {
                         insideItem = false;
                     }
-
+                    //going tyo the next item tag/news
                     eventType = sa.next();
                 }
 
 
             }
+            //something wrong with the URL we typed?
             catch (MalformedURLException e)
             {
                 exception = e;
             }
+            //Try to extract a data
             catch (XmlPullParserException e)
             {
                 exception = e;
             }
+            //Input / Output exception
             catch (IOException e)
             {
                 exception = e;
