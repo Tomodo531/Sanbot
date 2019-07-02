a1ea3ab39d636988e7d77646ccde874d9c4a7edf
Tomodo531 <Tomodo531@users.noreply.github.com>
2019-07-02 13:51:47 +0200

    Weather app added comments

--- app/src/main/java/com/example/sanbot/Weather.java
+++ app/src/main/java/com/example/sanbot/Weather.java
@@ -29,7 +29,6 @@ import java.util.Date;
 
 public class Weather extends AppCompatActivity {
 
-    public JSONArray WeatherArrayJson;
     //RequestQueue til volley
     public RequestQueue mQueue;
     //JsonObjectRequest til Volley
@@ -37,6 +36,7 @@ public class Weather extends AppCompatActivity {
     //Api Url
     public String ApiUrl;
 
+    //Laver en List til hver dag hvor i vejret for hver tredje time vil blive added
     ArrayList<JSONObject> WeatherListDary1 = new ArrayList<JSONObject>();
     ArrayList<JSONObject> WeatherListDary2 = new ArrayList<JSONObject>();
     ArrayList<JSONObject> WeatherListDary3 = new ArrayList<JSONObject>();
@@ -48,30 +48,45 @@ public class Weather extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        //Laver min en VOlley requestqueue
         mQueue = Volley.newRequestQueue(this);
+        //Url'en til Openweathermao key = f5297e2b15ba441c138473e483bbfb29
         ApiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=2618425&appid=f5297e2b15ba441c138473e483bbfb29&units=metric";
 
+        //Køre JsonParse som indeholder vores volley get function
         jsonParse();
     }
 
+    //Dette funktion bruges til at hente data fra vores vejr API. Det er også her hvor vi putter data'en fra hver enkelte dag ind i veres seperate lists.
     private void jsonParse(){
+
+        //Opretter vores Get Request.
         EventRequest = new JsonObjectRequest(Request.Method.GET, ApiUrl, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-
-                            WeatherArrayJson = response.getJSONArray("list");
+                            //Henter jsonarray'et som indeholder vejr info 5 dage frem samt ændringer i vejret hver tredje time.
+                            JSONArray WeatherArrayJson = response.getJSONArray("list");
+                            //Henter datoen for første dag og gemmer den til senere brug.
                             String dt_txt = WeatherArrayJson.getJSONObject(0).getString("dt_txt").substring(0,10) + "";
 
+                            //Int'en a bruges i nederstående switchcase til skifte imellem de forskellige lists som vi oprettede i starten som indeholder alt vejr data for hver enkelte dag
                             int a = 0;
 
+                            //Dette for loop checker alt data i vores WeatherArrayJson og sorterer det ind i de forskellioge lists
                             for(int i = 0; i < WeatherArrayJson.length(); i++){
+
+                                //Henter Json object on ligger i index i
                                 JSONObject WeatherObjectJson = WeatherArrayJson.getJSONObject(i);
+                                //Her henter vi datoen for dataen i overstående object. vi bruger en substring til at fjerne den sidste del af datoen som er tidspunktet fx. 2019/07/02 00:00:00 -> 2019/07/02.
+                                //Grunden til vi henter denne dato er så vi kan samligne den med dt_txt som er datoen for første dag. Vi gør dette så vi ved hvilken list den skal i.
                                 String dt_txtGet = WeatherObjectJson.getString("dt_txt").substring(0,10) + "";
 
+                                //Switchcase som udfra værdien af a ved hvilken list data'en skal puttes i.
                                 switch (a){
                                     case 0:
+                                        //Her sammenligner vi dt_txt og dt_txtGet for at se om den hører hjemme i nederstående list. hvis ikke ligges der 1 til a som får switchcasen til at gå videre til næste case.
                                         if (dt_txt.equals(dt_txtGet)){
                                             WeatherListDary1.add(WeatherObjectJson);
                                         }else{
@@ -118,12 +133,12 @@ public class Weather extends AppCompatActivity {
                                 }
                             }
 
-                            Log.d("Listss", "WeatherListDary1: " + WeatherListDary1);
-                            Log.d("Listss", "WeatherListDary2: " + WeatherListDary2);
-                            Log.d("Listss", "WeatherListDary3: " + WeatherListDary3);
-                            Log.d("Listss", "WeatherListDary4: " + WeatherListDary4);
-                            Log.d("Listss", "WeatherListDary5: " + WeatherListDary5);
+                            //Her kalder vi vores funktion ved navn InsertInput som putter vores hentede data ind i sperate cardviews for hver dag (5 dage).
+                            //I funktionen har vi 6 parametr hvor af DayTw, WeatherImg, TempTw, WeatherList er id'erne på de elementerne hvori data'en skal udskrives. DayAdditon er hvormange dage der skal ligges til
+                            //fx. hvis det er tirsdag idag og der står 1 i DayAdditon ligger den en dag til tirsdag så det bliver onsdag. Det sidste paramenter er vores list med vores data.
 
+
+                            //DayTw = TextView. Indeholder Ugedag. WeatherImg = ImageView. Indeholder Icon for vejret. TempTw = TextView. Indeholder temperaturen. WeatherList = ListView. Indeholder Vejr data for hver tredje time.
                             TextView DayTw = findViewById(R.id.DayTw1); ImageView WeatherImg = findViewById(R.id.WeatherImg1); TextView TempTw = findViewById(R.id.TempTw1); ListView WeatherList = findViewById(R.id.WeatherList1);
                             InsertInput(0, DayTw, WeatherImg, TempTw, WeatherList, WeatherListDary1);
 
@@ -156,20 +171,29 @@ public class Weather extends AppCompatActivity {
         mQueue.add(EventRequest);
     }
 
-    public int size = 0;
-
+    //InsertInput funktionen er funktionen som inserter vores data ind i vores elementer.
     public void InsertInput(int DayAddition, TextView DayTV, ImageView ImgV, TextView TempTV, ListView TempList, ArrayList<JSONObject> ArrayList) throws JSONException {
 
+        //Henter dato for dagen i vores list.
         String DateDate = ArrayList.get(0).getString("dt_txt").substring(0,10);
 
+        //Laver en SimpleDateFormat samt en ny Date variable som indeholder dags dato.
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
 
+        //Her bruger vi java.util.Date til at finde ud af hvilken ugedag vi har.
+
+        //Opretter en Calender variable.
         Calendar c = Calendar.getInstance();
+        //Sætter Calender til dags dato.
         c.setTime(date);
+        //Henter ugedag og ligger vores DayAddition til.
         int DayOfweek = c.get(Calendar.DAY_OF_WEEK) + DayAddition;
+
+        //Opretter String variablen day som vil indeholde vores ugedag i tesktform istedet for tal
         String Day = "";
 
+            //Switchcase som finder frem til vore ugedag i Tekst form
             switch (DayOfweek){
 
                 case 1:
@@ -201,34 +225,52 @@ public class Weather extends AppCompatActivity {
                     break;
             }
 
+        //Udskirver ugedagen i TextView'en DayTV
         DayTV.setText(Day);
 
+        //I disse to if else statements finder vi ud af om listen vi har fat i er listen for dags dato eller om det er en anden dag. gurnden til vi gør dette er fordi,
+        //hvis det er dags dato vil vi gerne udskrive hvordan vejret er lige nu og hvis det ikke er dags dato vil vi gerne udsrkive hvordan vejret er midt på dagen i vores tilfælde er det kl 15:00.
 
+        //Tjekker om det er dags dato
         if (DateDate.equals(dateFormat.format(date))){
+
+            //udskriver nuværende temperatur. "\u00B0" = °
             TempTV.setText(ArrayList.get(0).getJSONObject("main").getString("temp").substring(0,2) + "\u00B0");
 
+            //Her henter vi vejr iconet som representere vejret og tager det fra string til en int
             int resID = getResources().getIdentifier("img"+ArrayList.get(0).getJSONArray("weather").getJSONObject(0).getString("icon") , "drawable", getPackageName());
+            //Udsrkiver billedet.
             ImgV.setImageResource(resID);
         }else{
+            //Udsrkiver temperaturen midt på dagen 15:00
             TempTV.setText(ArrayList.get(4).getJSONObject("main").getString("temp").substring(0,2) + "\u00B0");
 
+            //Her henter vi vejr iconet som representere vejret og tager det fra string til en int
             int resID = getResources().getIdentifier("img"+ArrayList.get(4).getJSONArray("weather").getJSONObject(0).getString("icon") , "drawable", getPackageName());
+            //Udsrkiver billedet.
             ImgV.setImageResource(resID);
         }
 
+        //Her Udskriver vi vores resterende data altså vejret for hver tredje time. Vi gør dette ved at udsrkive det i en ListView ved hjælp af en Customadpater.
+
+        //Kalder vores Customadpater
         Customadpater customadpater = new Customadpater(ArrayList);
 
+        //Udskriver data'en i vores ListView. TempList er ListViewens id som var en af de 6 parameter fra tidligere.
         TempList.setAdapter(customadpater);
     }
 
     class Customadpater extends BaseAdapter {
 
+        //Opretter en ArrayList hvor vores data vil blive gemt
         ArrayList<JSONObject> ArraylistAdapter = null;
 
+        //contructor som henter data'en til ArrayList'en
         public Customadpater(ArrayList<JSONObject> Arraylist){
             ArraylistAdapter = Arraylist;
         }
 
+        //Finder længden af vores liste for at finde ud af hvor mange gange vi skal køre funktionen.
         @Override
         public int getCount() {
             return ArraylistAdapter.size();
@@ -244,24 +286,33 @@ public class Weather extends AppCompatActivity {
             return 0;
         }
 
+        //Her udskriver vi vores data i vores elementer
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
+            //Opretter vores View som er det layout som vi udsrkiver vores data i.
             convertView = getLayoutInflater().inflate(R.layout.weather_listitem, null);
+
+            //Opretter alle vores elementer
             TextView Timeitem = convertView.findViewById(R.id.Timeitem);
             TextView Tempitem = convertView.findViewById(R.id.Tempitem);
             ImageView WeatherImageItem = convertView.findViewById(R.id.WeatherImageItem);
 
             try {
+                //Henter og udsrkiver tidspunktet
                 Timeitem.setText(ArraylistAdapter.get(position).getString("dt_txt").substring(11));
+                //Henter og udsrkiver temperaturen
                 Tempitem.setText(ArraylistAdapter.get(position).getJSONObject("main").getString("temp"));
 
+                //Henter Icon navn og laver det om til et id
                 int resID = getResources().getIdentifier("img"+ArraylistAdapter.get(position).getJSONArray("weather").getJSONObject(0).getString("icon") , "drawable", getPackageName());
+                //Udskriver billedet
                 WeatherImageItem.setImageResource(resID);
             } catch (JSONException e) {
                 e.printStackTrace();
                 Log.d("Listss", "getView: Error");
             }
 
+            //Returner vores view
             return convertView;
         }
     }
